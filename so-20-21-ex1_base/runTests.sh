137636167b5cbad4f5b0d1081e683075bfd9fcf3
Hugo96870 <hugoaprita@gmail.com>
2020-10-25 12:50:57 +0000

    bash feita

---
+++ so-20-21-ex1_base/runTests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+inputdir=$1
+outputdir=$2
+maxthreads=$3
+cd $inputdir
+for test in *.txt
+do
+    for i in $(seq 1 $maxthreads)
+    do
+        cd ..
+        echo InputFile=$test NumThreads=$i
+        ./tecnicofs < tecnicofs "$inputdir/$test" "$outputdir"/"$test%.txt"-"$i".txt "$i" > auxiliar.txt
+        sentence=$( tail -n 1 auxiliar.txt)
+        rm auxiliar.txt
+        echo $sentence
+        cd $inputdir
+    done
+done
