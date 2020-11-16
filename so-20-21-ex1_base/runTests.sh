a39546d42f2018959031225d361dc6b04ffd5714
Hugo96870 <hugoaprita@gmail.com>
2020-11-16 16:49:10 +0000

    FIM SHELL

--- so-20-21-ex1_base/runTests.sh
+++ so-20-21-ex1_base/runTests.sh
@@ -3,31 +3,46 @@
 inputdir=$1
 outputdir=$2
 maxthreads=$3
-#verificar nr args
-if [ ! -d "$inputdir" ] ;
-then
-    echo "Input directory is not a directory\n"
-    exit 1
 
-elif [ ! -d "$outputdir" ] ;
-then
-    echo "Output directory is not a directory\n"
-    exit 1
+nrArgs=0
 
-elif [ $maxthreads -le 0 ] ;
+for var in "$@"
+do
+    (( nrArgs++ ))
+done
+
+if [ ! $nrArgs -eq 3 ]
+then 
+    echo "Invalid input"
+    exit 1
+fi
+
+if [ ! -d "$inputdir" ]
 then
-    echo "Invalid number of threads\n"
+    echo "Input directory is not a directory"
+    exit 1
+fi
+
+if [ ! -d "$outputdir" ]
+then
+    echo "Output directory is not a directory"
+    exit 1
+fi
+
+if [ $maxthreads -le 0 ]
+then
+    echo "Invalid number of threads"
     exit 1
 fi
 
 cd $inputdir
 for test in *.txt
 do
-    for i in $(seq 1 $maxthreads)
+    for nrThreads in $(seq 1 $maxthreads)
     do
         cd ..
-        echo InputFile=$test NumThreads=$i
-        ./tecnicofs "$inputdir/$test" "$outputdir"/"$test%.txt"-"$i".txt "$i" | tail -n 1   #print mau
+        echo InputFile=$test NumThreads=$nrThreads
+        ./tecnicofs "$inputdir/$test" "$outputdir"/${test%.*}-"$nrThreads".txt "$nrThreads" | tail -n 1
         cd $inputdir
     done
 done
